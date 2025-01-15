55b013b9769bbd0e889249e8d38ad1fb53eca3d1
Alysson Pereira | Grupo Moura <alysson.pereira@grupomoura.com>
2025-01-15 16:42:01 -0300

    /revendas ok

--- routes/distribuidores.py
+++ routes/distribuidores.py
@@ -6,22 +6,27 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from config.db import get_db_conn
 from services.geocode import geocode
+from services.valida_input import is_valid_lat_lon
 
 router = APIRouter(prefix='/distribuidores', tags=['distribuidores'])
 t_conn = Annotated[Connection, Depends(get_db_conn)]
 
 
 @router.get('/')
-def busca_distribuidores(conn: t_conn, endereco=None, latitude=None, longitude=None):
+def busca_distribuidores(conn: t_conn, endereco=None):  
     if endereco:
-        latitude, longitude = geocode(endereco)
-
-    if latitude is None or longitude is None:
+        coordenadas = is_valid_lat_lon(endereco)
+        if coordenadas:
+            latitude, longitude = coordenadas
+        else:
+            latitude, longitude = geocode(endereco)
+        cursor = conn.cursor()
+        distribuidores = cursor.execute(
+            'SELECT *, sqrt(pow(latitude - :x, 2) + pow(longitude - :y, 2)) AS distance FROM distribuidores ORDER BY distance LIMIT 5;'
+        , {'x': latitude, 'y': longitude}).fetchall()
+        return distribuidores
+    else:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail='É necessário fornecer latitude e longitude ou um endereço',
         )
-
-    cursor = conn.cursor()
-    distribuidores = cursor.execute('SELECT * FROM distribuidores;').fetchall()
-    return distribuidores
--- services/geocode.py
+++ services/geocode.py
@@ -1,14 +1,31 @@
-import requests
+# import requests
+
+
+# def geocode(endereco):
+#     api_url = 'https://nominatim.openstreetmap.org/search'
+#     params = {'q': endereco, 'format': 'json', 'limit': 1}
+#     response = requests.get(api_url, params=params)
+#     if response.status_code == 200 and response.json():
+#         resultado = response.json()[0]
+#         latitude = float(resultado['lat'])
+#         longitude = float(resultado['lon'])
+#         return latitude, longitude
+#     else:
+#         raise ValueError('Endereço não encontrado')
+
+
+
+import googlemaps
+
+gmaps = googlemaps.Client('')
 
 
 def geocode(endereco):
-    api_url = 'https://nominatim.openstreetmap.org/search'
-    params = {'q': endereco, 'format': 'json', 'limit': 1}
-    response = requests.get(api_url, params=params)
-    if response.status_code == 200 and response.json():
-        resultado = response.json()[0]
-        latitude = float(resultado['lat'])
-        longitude = float(resultado['lon'])
-        return latitude, longitude
+    geocode_result = gmaps.geocode(endereco)
+    if len(geocode_result) >= 1:
+        latitude = geocode_result[0]['geometry']['location']['lat']
+        longitude = geocode_result[0]['geometry']['location']['lng']
     else:
-        raise ValueError('Endereço não encontrado')
+        latitude = 0
+        longitude = 0
+    return (latitude, longitude)
